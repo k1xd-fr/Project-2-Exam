a3016a4883507e02cd5c95cd5d4ab73d80850d0f
sweetkurut <devxwc@gmail.com>
2023-03-27 09:53:10 +0600

    fixed slider

--- client/src/components/UI/Slider/Slider.jsx
+++ client/src/components/UI/Slider/Slider.jsx
@@ -1,20 +1,54 @@
 import styles from "./Slider.module.sass";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper.min.css";
 import Image from "next/image";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import images from "./index";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+
+// import required modules
+import { EffectCoverflow } from "swiper";
 
 const Slider = ({ items }) => {
+  const [images, setImages] = useState([
+    {
+      href: "/img/slider-img1.png",
+    },
+    {
+      href: "/img/slider-img1.png",
+    },
+    {
+      href: "/img/slider-img1.png",
+    },
+  ]);
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        
-        <Swiper>
-          {!!items.length && items.map(( item, index ) => (
-            
-            <SwiperSlide key={index}>
-              {/* <Image src={item.image} alt={item.title} width={1100} height={409} /> */}
-            </SwiperSlide>
-          ))}
+        <Swiper
+          effect={"coverflow"}
+          spaceBetween={30}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            scale: 0.9,
+            depth: 20,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          modules={[EffectCoverflow]}
+          className="mySwiper"
+        >
+          {images.map((item) => {
+            return (
+              <SwiperSlide>
+                <Image src={item.href} width={1110} height={410} />
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </div>
     </div>
