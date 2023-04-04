b2d67b9c9f734d6a35570f9468c76af8d04ec771
sweetkurut <devxwc@gmail.com>
2023-04-04 09:59:48 +0600

    aaa

--- client/src/components/User/UserBonus/index.jsx
+++ client/src/components/User/UserBonus/index.jsx
@@ -4,14 +4,16 @@ import styles from "./style.module.sass";
 
 export const UserBonus = () => {
   return (
-    <div className={styles.container}>
-      <h1>Мои бонусы</h1>
-      <div className={styles.cards}>
-        <BonusCard />
+    <div className={styles.containerBg}>
+      <div className={styles.containerProfile}>
+        <h1>Мои бонусы</h1>
+        <div className={styles.cards}>
+          <BonusCard />
+        </div>
+        <Link href="/action" className={styles.link}>
+          Все наши акции
+        </Link>
       </div>
-      <Link href="/action" className={styles.link}>
-        Все наши акции
-      </Link>
     </div>
   );
 };
