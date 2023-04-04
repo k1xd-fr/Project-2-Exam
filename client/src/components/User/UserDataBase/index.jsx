134d70ab95e364bd243aa36a125e2fadfc409be8
sweetkurut <devxwc@gmail.com>
2023-04-04 09:41:40 +0600

    gg wp

--- client/src/components/User/UserDataBase/index.jsx
+++ client/src/components/User/UserDataBase/index.jsx
@@ -27,66 +27,68 @@ const UserDataBase = ({ user }) => {
 
   return (
     <div className={styles.container}>
-      <h1>Личные данные</h1>
+      <div className={styles.profiler}>
+        <h1>Личные данные</h1>
 
-      {/* {authData.username} */}
+        {/* {authData.username} */}
 
-      <form className={styles.form}>
-        <div className={styles.userData}>
-          <Input
-            style={styles.userDataInput}
-            type="text"
-            name="name"
-            placeholder={user}
-            title="имя"
-            onchahge={change}
-            value={param.name}
-          />
-          <Input
-            style={styles.userDataInput}
-            type="email"
-            name="email"
-            placeholder={user}
-            title="e-mail"
-            onchahge={change}
-            value={param.email}
-          />
-          <Input
-            style={styles.userDataInput}
-            type="date"
-            name="date"
-            placeholder={user}
-            title="Дата рождения"
-            onchahge={change}
-            value={param.date}
-          />
-          <Input
-            style={styles.userDataInput}
-            type="num"
-            name="num"
-            placeholder={user}
-            title="Номер телефона"
-            onchahge={change}
-            value={param.num}
-          />
-          <Input
-            style={styles.userDataInput}
-            type="password"
-            name="password"
-            placeholder={user}
-            title="Пароль"
-            onchahge={change}
-            value={param.password}
-          />
-        </div>
-        <h2>Подписки</h2>
-        <div className={styles.Sms}>
-          <input type="radio" name="sms" id="sms" />
-          <p>Сообщать о бонусах, акциях и новых продуктах</p>
-          <span>i</span>
-        </div>
-        <Button style={styles.btn}>Выйти</Button>
-      </form>
+        <form className={styles.form}>
+          <div className={styles.userData}>
+            <Input
+              style={styles.userDataInput}
+              type="text"
+              name="name"
+              placeholder={user}
+              title="имя"
+              onchahge={change}
+              value={param.name}
+            />
+            <Input
+              style={styles.userDataInput}
+              type="email"
+              name="email"
+              placeholder={user}
+              title="e-mail"
+              onchahge={change}
+              value={param.email}
+            />
+            <Input
+              style={styles.userDataInput}
+              type="date"
+              name="date"
+              placeholder={user}
+              title="Дата рождения"
+              onchahge={change}
+              value={param.date}
+            />
+            <Input
+              style={styles.userDataInput}
+              type="num"
+              name="num"
+              placeholder={user}
+              title="Номер телефона"
+              onchahge={change}
+              value={param.num}
+            />
+            <Input
+              style={styles.userDataInput}
+              type="password"
+              name="password"
+              placeholder={user}
+              title="Пароль"
+              onchahge={change}
+              value={param.password}
+            />
+          </div>
+          <h2>Подписки</h2>
+          <div className={styles.Sms}>
+            <input type="radio" name="sms" id="sms" />
+            <p>Сообщать о бонусах, акциях и новых продуктах</p>
+            <span>i</span>
+          </div>
+          <Button style={styles.btn}>Выйти</Button>
+        </form>
+      </div>
     </div>
   );
 };
